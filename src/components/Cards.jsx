22dd682443e78a713c14a8c84e0725d1784c0808
Himanshu <himanshu_196018@saitm.org>
2023-11-13 22:48:50 +0530

    section 1 syling completed

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,9 +3,18 @@ const Cards = () => {
     <div className="section1">
       <div className="card-front">
         <img src="/images/bg-card-front.png" alt="card front" />
+        <div className="card-front-info">
+          <img src="/images/card-logo.svg" />
+          <p className="card-number">{"0000 0000 0000 0000"}</p>
+          <p className="bottom-info">
+            <span>{"JANE APPLESEED"}</span>
+            <span>{"00/00"}</span>
+          </p>
+        </div>
       </div>
       <div className="card-back">
         <img src="/images/bg-card-back.png" alt="card back" />
+        <p className="card-back-info">{"000"}</p>
       </div>
     </div>
   );
