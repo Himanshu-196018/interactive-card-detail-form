685ad3e10062ce9d46c4b6d4f60e2cdc761cdf37
Himanshu <himanshu_196018@saitm.org>
2023-11-14 23:28:15 +0530

    styled form

--- src/components/DateInput.jsx
+++ src/components/DateInput.jsx
@@ -2,8 +2,10 @@ const DateInput = ({ name, ph, type }) => {
   return (
     <label htmlFor={name.slice(0, 3)}>
       {name.toUpperCase()}
-      <input name={name.slice(0, 3)} type={type} placeholder={ph[0]} />
-      <input name={name.slice(0, 3)} type={type} placeholder={ph[1]} />
+      <div className="flex-container">
+        <input name={name.slice(0, 3)} type={type} placeholder={ph[0]} />
+        <input name={name.slice(0, 3)} type={type} placeholder={ph[1]} />
+      </div>
     </label>
   );
 };
