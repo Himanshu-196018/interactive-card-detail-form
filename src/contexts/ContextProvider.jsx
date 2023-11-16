bf0630477a006905ba152f42a4d0629e140355fe
Himanshu <himanshu_196018@saitm.org>
2023-11-16 23:52:38 +0530

    adding context to manage state

---
+++ src/contexts/ContextProvider.jsx
@@ -0,0 +1,9 @@
+import { createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+
+export const ContextProvider = ({ children }) => {
+  return <StateContext.Provider value={{}}>{children}</StateContext.Provider>;
+};
+
+export const useStateContext = () => useContext(StateContext);
